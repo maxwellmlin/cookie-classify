65ba06f2897489d33c82b5af8e0f6eba1ec5b0bd
Maxwell Lin <maxwell.lin@duke.edu>
2023-06-22 14:46:10 -0400

    Finish (hopefully)

--- main.py
+++ main.py
@@ -12,33 +12,36 @@ options = FirefoxOptions()
 options.add_argument("--headless")  # TODO: get native working
 
 driver = webdriver.Firefox(options=options)
-cookie_header = CookieRequestHeader()
 
 
 def remove_necessary_interceptor(request):
     """
-    Interceptor that removes all necessary cookies from the GET request.
-    """
-    cookie_header.load_header(request.headers["Cookie"], request.url)
-    cookie_header.remove_necessary()
+    Interceptor that removes all necessary cookies from a GET request.
 
+    `request` is the GET request.
+    """
+    if request.headers.get("Cookie") is None:
+        return
+
+    cookie_header = CookieRequestHeader(request.url, request.headers["Cookie"])  # TODO: Is the URL correct?
+    cookie_header.remove_necessary()
     modified_header = cookie_header.get_header()
 
     request.headers["Cookie"] = modified_header
 
 
 """
-Initial crawl obtains cookies
+Initial crawl to collect all site cookies
 """
 driver.get(SITE_URL)
 driver.save_full_page_screenshot(SCREENSHOTS_PATH + "initial_crawl.png")
 
 """
-Second crawl removes all necessary cookies
+Second crawl without necessary cookies
 """
 
 # Set the interceptor on the driver
 driver.request_interceptor = remove_necessary_interceptor
 
 driver.get(SITE_URL)
-driver.save_full_page_screenshot(SCREENSHOTS_PATH + "second_crawl.png")
+driver.save_full_page_screenshot(SCREENSHOTS_PATH + "remove_necessary_crawl.png")
