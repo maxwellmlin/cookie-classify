fe6005530acfb4105e5f9f01d50429f298ee23ea
Maxwell Lin <maxwell.lin@duke.edu>
2023-06-22 14:13:51 -0400

    Update docstrings

--- cookiescript.py
+++ cookiescript.py
@@ -2,16 +2,23 @@ import json
 
 
 class CookieScript:
+    """
+    Related functions for CookieScript database lookup.
+    """
+
     def __init__(self) -> None:
         self.cookiescript = self.load_cookie_script()
 
     def load_cookie_script(self, cookie_script_path="inputs/cookiescript.json"):
         """
-        Returns Cookie-Script JSON file as a dictionary.
+        Loads Cookie-Script JSON file as a dictionary.
 
-        The JSON file is reformatted as relevant key-value pairs.
-        i.e., cookie_data = cookie_script[site_url][cookie_name]
+        `cookie_script_path` is the path of the Cookie-Script JSON file.
+
+        To obtain a cookie from the dictionary, use:
+        cookie_data = cookiescript[domain][cookie_key]
         """
+
         object_list = []
         with open(cookie_script_path) as file:
             for line in file:
@@ -21,29 +28,51 @@ class CookieScript:
         cookiescript = {}
 
         for object_ in object_list:
-            # Extract the website and cookies from the current JSON object
-            website = object_["website"]
+            # Extract the domain and cookies from the current JSON object
+            domain = object_["website"]
             cookies = object_["cookies"]
 
             # Create a nested dictionary for the current website
-            cookiescript[website] = {}
+            cookiescript[domain] = {}
 
             # Populate the nested dictionary with cookie data for the current website
             for cookie in cookies:
-                cookie_key = cookie.pop(
-                    "cookieKey"
-                )  # Remove 'cookieKey' from the dictionary values and assign it as a key
-                cookiescript[website][cookie_key] = cookie
+                cookie_key = cookie.pop("cookieKey")
+                cookiescript[domain][cookie_key] = cookie
 
         return cookiescript
 
-    def get_cookie_class(self, domain, cookie_name):
+    def get_cookie_class(self, domain, cookie_key):
+        """
+        Returns the class of the given cookie.
+
+        `domain` is the 2nd-level and top-level domain (e.g., 'google.com')
+        `cookie_key` is the name of the cookie
+
+        The class will be one of the following strings:
+        - "Strictly Necessary"
+        - "Performance"
+        - "Functionality"
+        - "Targeting"
+        - "Unclassified"
+        """
+
         if not (domain_cookies := self.cookie_script.get(domain)):
             # domain not in cookie_script
             return "Unclassified"
 
-        if not (cookie := domain_cookies.get(cookie_name)):
-            # cookie_name not in cookie_script
+        if not (cookie := domain_cookies.get(cookie_key)):
+            # cookie_key not in cookie_script
             return "Unclassified"
 
         return cookie["class"]
+
+    def is_necessary(self, domain, cookie_key):
+        """
+        Returns whether the given cookie is Strictly Necessary.
+
+        `domain` is the 2nd-level and top-level domain (e.g., 'google.com')
+        `cookie_key` is the name of the cookie
+        """
+
+        return self.get_cookie_class(domain, cookie_key) == "Strictly Necessary"
