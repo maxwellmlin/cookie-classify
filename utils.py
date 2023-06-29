3359a000ab7082deac06dcb6a3c8379f404de3ba
Maxwell Lin <maxwell.lin@duke.edu>
2023-06-29 10:52:46 -0400

    Refactor

--- utils.py
+++ utils.py
@@ -32,10 +32,8 @@ def get_full_domain(url: str) -> str:
         full domain of `url`.
     """
     separated_url = tldextract.extract(url)
-    full_domain = f"{separated_url.subdomain}.{separated_url.domain}.{separated_url.suffix}"
 
-    # Remove leading '.' if subdomain is empty
-    if full_domain[0] == ".":
-        full_domain = full_domain[1:]
-
-    return full_domain
+    if separated_url.subdomain == "":
+        return get_domain(url)
+    else:
+        return f"{separated_url.subdomain}.{separated_url.domain}.{separated_url.suffix}"
