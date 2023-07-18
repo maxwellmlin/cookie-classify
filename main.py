06ee062ed1904ef3c5ec8df01f22f10bd18fffe0
Maxwell Lin <maxwell.lin@duke.edu>
2023-07-18 09:16:25 -0400

    Revert to sequential

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def worker(data_path, site_url, depth):
     crawler.quit()
 
 
-SITE_LIST_PATH = "inputs/sites/detectedBanner.txt"  # Path to list of sites to crawl
+SITE_LIST_PATH = "inputs/sites/sites.txt"  # Path to list of sites to crawl
 
 if not os.path.exists("crawls"):
     os.mkdir("crawls")
@@ -23,17 +23,14 @@ with open(SITE_LIST_PATH) as file:
     for line in file:
         sites.append(line.strip())
 
-with mp.Pool(processes=mp.cpu_count()) as pool:
-    processes = []
+for site_url in sites:
+    # TODO: this is a temp fix for detectedBanner.txt
+    site_url = f"https://{site_url}"
 
-    for site_url in sites:
-        # TODO: this is a temp fix for detectedBanner.txt
-        site_url = f"https://{site_url}"
+    # Create data folder
+    data_path = f"crawls/{utils.get_domain(site_url)}/"
 
-        # Create data folder
-        data_path = f"crawls/{utils.get_domain(site_url)}/"
-
-        args = (data_path, site_url, 2)
-        processes.append(args)
-
-    pool.starmap(worker, processes)
+    # See https://stackoverflow.com/a/1316799/ for why we need to use multiprocessing
+    process = mp.Process(target=worker, args=(data_path, site_url, 2))
+    process.start()
+    process.join()
