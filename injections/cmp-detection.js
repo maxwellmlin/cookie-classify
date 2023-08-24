c14275b0bbf7c75e86a19ba2c5a19e330019e505
Maxwell <maxwell.lin@duke.edu>
2023-08-24 13:49:12 -0400

    Update injections

--- injections/cmp-detection.js
+++ injections/cmp-detection.js
@@ -1,9 +1,15 @@
 /**
- * Return the name of the CMP used on the page
- * @returns {string} "onetrust"
+ * Return the CMP(s) used on the page.
+ * @returns {string[]} An array of API names that are exposed on the `window` object. May be empty.
  */
 
-// OneTrust
-if (!!window.OneTrust) {
-  return "onetrust";
+possibleAPIs = ["OneTrust", "__tcfapi"];
+
+activeAPIs = [];
+for (let name of possibleAPIs) {
+  if (!!window[name]) {
+    activeAPIs.push(name);
+  }
 }
+
+return activeAPIs;
