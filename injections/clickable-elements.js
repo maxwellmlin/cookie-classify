9e64ca36ac23bcf8dcc0e9392c0ade10fe296f9a
Maxwell <maxwell.lin@duke.edu>
2023-09-09 20:42:39 -0400

    Finish

--- injections/clickable-elements.js
+++ injections/clickable-elements.js
@@ -7,52 +7,22 @@ DEBUG = false
  * - <button> elements
  * - <a> elements
  * - Elements with an onclick attribute
- * - Elements with a cursor style of pointer
+ * - Elements with a pointer cursor style
  * 
  * Adapted from: https://gist.github.com/iiLaurens/81b1b47f6259485c93ce6f0cdd17490a
  * 
  * @returns {string[]} CSS selectors for clickable elements.
  */
 
-window.scrollTo(0, 0)
-var bodyRect = document.body.getBoundingClientRect();
-
 var items = Array.prototype.slice.call(
     document.querySelectorAll('*')
 ).map(function (element) {
-    var rect = element.getBoundingClientRect();
     return {
         element: element,
         include: (element.tagName === "BUTTON" || element.tagName === "A" || (element.onclick != null) || window.getComputedStyle(element).cursor == "pointer"),
-        rect: {
-            left: Math.max(rect.left - bodyRect.x, 0),
-            top: Math.max(rect.top - bodyRect.y, 0),
-            right: Math.min(rect.right - bodyRect.x, document.body.clientWidth),
-            bottom: Math.min(rect.bottom - bodyRect.y, document.body.clientHeight)
-        },
     };
 }).filter(item =>
     item.include);
-
-// Only keep inner clickable items
-// items = items.filter(x => !items.some(y => x.element.contains(y.element) && !(x == y)))
-
-if (DEBUG) {
-    // Lets create a floating border on top of these elements that will always be visible
-    items.forEach(function (item) {
-        newElement = document.createElement("div");
-        newElement.style.outline = "2px dashed rgba(255,0,0,.75)";
-        newElement.style.position = "absolute";
-        newElement.style.left = item.rect.left + "px";
-        newElement.style.top = item.rect.top + "px";
-        newElement.style.width = (item.rect.right - item.rect.left) + "px";
-        newElement.style.height = (item.rect.bottom - item.rect.top) + "px";
-        newElement.style.pointerEvents = "none";
-        newElement.style.boxSizering = "border-box";
-        newElement.style.zIndex = 2147483647;
-        document.body.appendChild(newElement);
-    })
-}
 
 // See: https://stackoverflow.com/a/4588211
 function fullPath(el) {
