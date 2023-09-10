3657ca3b18f70641625812b2ae30cdd299ae52e3
Maxwell <maxwell.lin@duke.edu>
2023-09-09 22:27:58 -0400

    Sync

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 CRAWL_NAME = "clickstream"  # Name of crawl
-SITE_LIST_PATH = "inputs/sites/sites.txt"  # Path to list of sites to crawl
+SITE_LIST_PATH = "inputs/sites/detectedBanner.txt"  # Path to list of sites to crawl
 
 CRAWL_PATH = f"crawls/{CRAWL_NAME}/"
 LOGGER_NAME = "cookie-classify"
