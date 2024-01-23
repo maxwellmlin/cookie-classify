32c5cdc305ef6939727cf694fbd278977446fb56
Maxwell Lin <maxwell.lin@duke.edu>
2024-01-23 09:40:59 -0500

    Add slurm

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import json
 import logging
 import multiprocessing as mp
 import pathlib
+import os
+import argparse
+from filelock import Timeout, FileLock
 
 from crawler import Crawler, CrawlDataEncoder
 import config
@@ -14,11 +17,12 @@ def worker(site_url: str, queue: mp.Queue) -> None:
 
     # result = crawler.compliance_algo(config.DEPTH)
     result = crawler.classification_algo(num_clickstreams=10, clickstream_length=5, control_screenshots=5)
+    # result = crawler.classification_algo(num_clickstreams=1, clickstream_length=1, control_screenshots=1)
 
     queue.put(result)
 
 
-def main():
+def main(jobs=1):
     pathlib.Path(config.CRAWL_PATH).mkdir(parents=True, exist_ok=True)
 
     logger.setLevel(logging.INFO)
@@ -46,19 +50,45 @@ def main():
     # Create input for pool
     output = mp.Queue()
     data = {}
-    for site_url in sites:
-        process = mp.Process(target=worker, args=(f"https://{site_url}", output))
+
+    SLURM_ARRAY_TASK_ID = int(os.getenv('SLURM_ARRAY_TASK_ID'))
+    print("SLURM_ARRAY_TASK_ID:", SLURM_ARRAY_TASK_ID)
+
+    results_path = config.CRAWL_PATH + 'results.json'
+    with open(results_path, 'w') as results:
+        results.write("{}")
+
+    lock_path = results_path + '.lock'
+
+    lock = FileLock(lock_path, timeout=1)
+
+    for i in range(SLURM_ARRAY_TASK_ID-1, len(sites), jobs):
+        process = mp.Process(target=worker, args=(f"https://{sites[i]}", output))
         process.start()
 
         result = output.get()
-        key = result.pop('data_path')
+
+        # Read existing data, update it, and write back
+        with lock:
+            with open(results_path, 'r') as results:
+                data = json.load(results)
+
+        key = result.pop('url')
         data[key] = result
 
-        with open(config.CRAWL_PATH + 'results.json', 'w') as log_file:
-            json.dump(data, log_file, cls=CrawlDataEncoder)
+        with lock:
+            with open(results_path, 'w') as results:
+                json.dump(data, results, cls=CrawlDataEncoder)
 
         process.join()
 
-
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Start a crawl.")
+    parser.add_argument(
+        '--jobs',
+        type=int,
+        help="Number of jobs to create."
+    )
+    args = parser.parse_args()
+    
+    main(jobs=args.jobs)
---
+++ run.py
@@ -0,0 +1,31 @@
+import os
+
+slurm_log_path = 'slurm_logs'
+shFileName = '.temp_run.sh'
+
+if not os.path.exists(slurm_log_path):
+    os.mkdir(slurm_log_path)
+
+def sbatchRun(command, commandName, jobs=1, memory=4):
+    shFile = [
+        "#!/bin/bash",
+        "#SBATCH --array=1-%d" % jobs,
+        "#SBATCH --cpus-per-task=2",
+        "#SBATCH --mem-per-cpu=%dG" % memory,
+        "#SBATCH --job-name=%s" % commandName,
+        "#SBATCH -o {slurm_log_path}/slurm-%j.out",
+
+        "eval \"$(command conda 'shell.bash' 'hook' 2> /dev/null)\"",
+        "conda activate cookie-classify",
+        
+        command
+        ]
+
+    with open(shFileName, 'w') as f:
+        f.write('\n'.join(shFile))
+
+    print(f'Running {commandName}.')
+    os.system('sbatch %s' % shFileName)
+
+jobs = 5
+sbatchRun(f'python3 -u main.py --jobs {jobs}', commandName='cookie', jobs=jobs)
