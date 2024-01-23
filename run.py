75aa0506c3c5e0a4de80d44abdcadcf2627a9527
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-01-23 10:03:39 -0500

    Remove stdio

--- run.py
+++ run.py
@@ -13,7 +13,8 @@ def sbatchRun(command, commandName, jobs=1, memory=4):
         "#SBATCH --cpus-per-task=2",
         "#SBATCH --mem-per-cpu=%dG" % memory,
         "#SBATCH --job-name=%s" % commandName,
-        f"#SBATCH -o {slurm_log_path}/slurm-%j.out",
+        # f"#SBATCH -o {slurm_log_path}/slurm-%j.out",
+        '#SBATCH -e {slurm_log_path}/slurm-%j.err'
 
         "eval \"$(command conda 'shell.bash' 'hook' 2> /dev/null)\"",
         "conda activate cookie-classify",
