1f239c5c9e573693ee2e3cb3a50ebc0632695cda
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-01-28 18:04:02 -0500

    Add more args

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-CRAWL_NAME = "slurm-test"  # Name of crawl
+CRAWL_NAME = "stupid"  # Name of crawl
 SITE_LIST_PATH = "inputs/sites/sites.txt"  # Path to list of sites to crawl
 DEPTH = 0
+
+NUM_CLICKSTREAMS = 10
+CLICKSTREAM_LENGTH = 5
 
 CRAWL_PATH = f"crawls/{CRAWL_NAME}/"
 LOGGER_NAME = "cookie-classify"
