c25c712b0f7beeb5f6908cd76c5ca909fa1da80a
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-01-29 12:06:05 -0500

    prep

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-CRAWL_NAME = "jan-28"  # Name of crawl
-SITE_LIST_PATH = "inputs/sites/sites.txt"  # Path to list of sites to crawl
+CRAWL_NAME = "3N76L"  # Name of crawl
+SITE_LIST_PATH = "inputs/sites/3N76L-top-250.txt"  # Path to list of sites to crawl
 DEPTH = 0
 TIME_TO_WAIT = 5
 
