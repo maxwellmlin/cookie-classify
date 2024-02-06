d40e38b79e4a94793e2b80289a2963a97ccf1fc1
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-02-06 09:50:15 -0500

    Revert

--- injections/clickable-elements.js
+++ injections/clickable-elements.js
@@ -47,12 +47,17 @@ function determineType(element) {
 function fullPath(el) {
     var names = [];
     while (el.parentNode) {
-        if (el == el.ownerDocument.documentElement) names.unshift(el.tagName);
-        else {
-            for (var c = 1, e = el; e.previousElementSibling; e = e.previousElementSibling, c++);
-            names.unshift(el.tagName + ":nth-child(" + c + ")");
+        if (el.id) {
+            names.unshift('#' + el.id);
+            break;
+        } else {
+            if (el == el.ownerDocument.documentElement) names.unshift(el.tagName);
+            else {
+                for (var c = 1, e = el; e.previousElementSibling; e = e.previousElementSibling, c++);
+                names.unshift(el.tagName + ":nth-child(" + c + ")");
+            }
+            el = el.parentNode;
         }
-        el = el.parentNode;
     }
     return names.join(" > ");
 }
