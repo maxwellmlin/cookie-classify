ba0fbfc0633b8c75658209b8d34e92e11260efb9
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-02-05 22:02:04 -0500

    Don't use ID in css selector

--- injections/clickable-elements.js
+++ injections/clickable-elements.js
@@ -47,17 +47,12 @@ function determineType(element) {
 function fullPath(el) {
     var names = [];
     while (el.parentNode) {
-        if (el.id) {
-            names.unshift('#' + el.id);
-            break;
-        } else {
-            if (el == el.ownerDocument.documentElement) names.unshift(el.tagName);
-            else {
-                for (var c = 1, e = el; e.previousElementSibling; e = e.previousElementSibling, c++);
-                names.unshift(el.tagName + ":nth-child(" + c + ")");
-            }
-            el = el.parentNode;
+        if (el == el.ownerDocument.documentElement) names.unshift(el.tagName);
+        else {
+            for (var c = 1, e = el; e.previousElementSibling; e = e.previousElementSibling, c++);
+            names.unshift(el.tagName + ":nth-child(" + c + ")");
         }
+        el = el.parentNode;
     }
     return names.join(" > ");
 }
