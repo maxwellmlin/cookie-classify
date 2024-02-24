237d8a4361b21165cb156584ef003230b94a2bd8
Maxwell Lin <28914712+maxwellmlin@users.noreply.github.com>
2024-02-23 21:20:52 -0500

    Sync

--- inject.py
+++ inject.py
@@ -2,23 +2,35 @@ import config
 import json
 from filelock import FileLock
 
-SITES_TO_INJECT = ["weather.com"]
+SITES_TO_INJECT = [
+    "arstechnica.com",
+    "hbomax.com",
+    "nodejs.org",
+    "engadget.com",
+    "safety.google"
+]
 print(SITES_TO_INJECT)
 
-if input("This is a destructive action if you are injecting duplicates. Are you sure you want to continue? (y/n) ") != "y":
+if (
+    input(
+        "This is a destructive action if you are injecting duplicates. Are you sure you want to continue? (y/n) "
+    )
+    != "y"
+):
     print("Exiting.")
     exit(0)
 
 
-queue_lock = FileLock(config.QUEUE_PATH + '.lock', timeout=10)
+queue_lock = FileLock(config.QUEUE_PATH + ".lock", timeout=10)
 
 with queue_lock:
-    with open(config.QUEUE_PATH, 'r') as file:
+    with open(config.QUEUE_PATH, "r") as file:
         sites = json.load(file)
-    
-    sites[:0] = SITES_TO_INJECT
-    
-    with open(config.QUEUE_PATH, 'w') as file:
+
+    sites.extend(SITES_TO_INJECT)
+    # sites[:0] = SITES_TO_INJECT
+
+    with open(config.QUEUE_PATH, "w") as file:
         json.dump(sites, file)
 
 print("Injection complete.")
